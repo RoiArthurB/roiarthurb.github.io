1411bc96c38adfaee8c35d24970acdd86848621b
RoiArthurB <contact@arthurbrugiere.fr>
2023-10-03 10:00:07 +0700

    Turn website as doc-only

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,19 +38,14 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/', 
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/RoiArthurB/roiarthurb.github.io/tree/main/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/RoiArthurB/roiarthurb.github.io/tree/main/',
-        },
+        blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
