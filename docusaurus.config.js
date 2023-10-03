4cf883a6a4ef6410fbe0543b3cb8ad55ab7a8bb4
RoiArthurB <contact@arthurbrugiere.fr>
2023-10-03 15:38:36 +0700

    Add per page last updated time and author name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,6 +44,8 @@ const config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/RoiArthurB/roiarthurb.github.io/tree/main/',
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
         },
         blog: false,
         theme: {
