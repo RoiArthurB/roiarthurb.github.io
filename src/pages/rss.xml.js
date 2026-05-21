de22115a1a000ab0ccf662467907bd2545569753
RoiArthurB <contact@arthurbrugiere.fr>
2026-05-21 13:47:27 +0700

    fix: RSS shows only published blog article

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,7 @@ import rss from '@astrojs/rss';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const posts = (await getCollection('blog')).filter((post) => post.data.publish === true);
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
