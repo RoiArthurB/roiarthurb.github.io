5c1eccf2e88495c5b408d835636c73c2bceb3651
RoiArthurB <contact@arthurbrugiere.fr>
2026-05-26 14:52:20 +0700

    chore: Reword some forgotten part Subtitle was still wrongly set from the template.

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Arthur Brugière';
-export const SITE_DESCRIPTION = 'Computer Researcher / IT Nerd';
+export const SITE_DESCRIPTION = 'Computer Researcher / IT Geek';
 
 export const CV_URL = '/resume.pdf';
 
