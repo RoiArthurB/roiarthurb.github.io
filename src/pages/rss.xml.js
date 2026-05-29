5f7f808fb0a713a16d8162e8e2ab7a3fa876186a
RoiArthurB <contact@arthurbrugiere.fr>
2026-05-29 21:37:37 +0700

    feat: Make the RSS feed embed the full article More conveniant for RSS readers

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,9 @@
 import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
+import MarkdownIt from 'markdown-it';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+
+const md = new MarkdownIt();
 
 export async function GET(context) {
 	const posts = (await getCollection('blog')).filter((post) => post.data.publish === true);
@@ -11,6 +14,7 @@ export async function GET(context) {
 		items: posts.map((post) => ({
 			...post.data,
 			link: `/blog/${post.id}/`,
+			content: md.render(post.body),
 		})),
 	});
 }
